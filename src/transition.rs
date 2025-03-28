b7193f9bee55658354b0af0f6def44c13aea16e5
smb123w64gb <smb123w64gb@gmail.com>
2025-03-28 04:55:07 -0700

    Well looks like mipskipin is needed for map compat... Halfing a myth

--- src/transition.rs
+++ src/transition.rs
@@ -30,10 +30,12 @@ pub fn vtf2xtf<P: AsRef<Path>>(input:P,output:P,half:bool){
     let mut resolution = (infile.hdr.width,infile.hdr.height);
     println!("{:?}",infile.hdr.image_format);
     let mut newmips = infile.mips;
+    let mut mipskip = 0;
     for a in &mut newmips{
         if half && a.level.len() > 1{
             a.pop();
-            resolution = (a.level[a.level.len()-1].resolution.0.try_into().unwrap(),a.level[a.level.len()-1].resolution.1.try_into().unwrap())
+            resolution = (a.level[a.level.len()-1].resolution.0.try_into().unwrap(),a.level[a.level.len()-1].resolution.1.try_into().unwrap());
+            mipskip = 1; //Technicly we could go further!
         }
         a.reverse();
         
@@ -44,7 +46,7 @@ pub fn vtf2xtf<P: AsRef<Path>>(input:P,output:P,half:bool){
         false => {lw=infile.hdr.low_res_image_width;lh=infile.hdr.low_res_image_height;lh = 0;mip_helper::Mip{resolution:((infile.hdr.low_res_image_width).into(),(infile.hdr.low_res_image_height).into()),img_data:(None)}},
         true => {lw=infile.hdr.low_res_image_width;lh=infile.hdr.low_res_image_height;infile.low_res}
     };
-    let mut outfile = XTFFile{hdr:(XTFHdr{ version: ((5,0)), header_size: (58),image_data_offset:(0x200),mip_skip_count:(0),pad:(0), width: (resolution.0), height: (resolution.1), flags: (infile.hdr.flags), num_frames: (infile.hdr.num_frames), preload_size: (0), reflectivity: (Vector { x: (infile.hdr.reflectivity.x), y: (infile.hdr.reflectivity.y), z: (infile.hdr.reflectivity.z) }), bump_scale: (infile.hdr.bump_scale), image_format: (infile.hdr.image_format),low_res_image_width:(0),low_res_image_height:(0), fallback_res_image_width: (lw), fallback_res_image_height: (lh), depth: (if infile.hdr.depth>0 {infile.hdr.depth} else {1}) })
+    let mut outfile = XTFFile{hdr:(XTFHdr{ version: ((5,0)), header_size: (58),image_data_offset:(0x200),mip_skip_count:(mipskip),pad:(0), width: (resolution.0), height: (resolution.1), flags: (infile.hdr.flags), num_frames: (infile.hdr.num_frames), preload_size: (0), reflectivity: (Vector { x: (infile.hdr.reflectivity.x), y: (infile.hdr.reflectivity.y), z: (infile.hdr.reflectivity.z) }), bump_scale: (infile.hdr.bump_scale), image_format: (infile.hdr.image_format),low_res_image_width:(0),low_res_image_height:(0), fallback_res_image_width: (lw), fallback_res_image_height: (lh), depth: (if infile.hdr.depth>0 {infile.hdr.depth} else {1}) })
     ,mips:(newmips),low_res:(low_res)};
     outfile.write(&mut BufWriter::new(std::fs::File::create(output).unwrap()));
 }
